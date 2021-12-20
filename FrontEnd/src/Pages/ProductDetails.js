ad2fa66c584468399249b9be09eda4e439708938
Tiger <danmats140@gmail.com>
2021-12-19 21:42:45 -0300

    Borrado de archivos

--- FrontEnd/src/Pages/ProductDetails.js
+++
@@ -1,9 +0,0 @@
-import Details from "../Components/Details"
-import "./ProductDetails.css"
-
-function ProductDetail(props) {
-    return (
-        <Details idItemAMostrar={props.match.params.id}></Details>
-    )
-}
-export default ProductDetail
