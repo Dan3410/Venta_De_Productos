8ca86d65ecb1fb5715837b789bcaab3efbd66835
Tiger <danmats140@gmail.com>
2022-01-06 22:47:26 -0300

    Fix: ahora al recargar la pagina no se redirecciona a la pagina de inicio

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -4,13 +4,12 @@ import Register from "./Pages/Register/Register.js";
 import Login from "./Pages/Login/Login.js";
 import ProductDetail from "./Pages/ProductDetails/ProductDetails.js";
 import Profile from "./Pages/Profile/Profile.js";
-import Gestion_Productos from "./Pages/Gestion_Producto/Gestion_Productos.js"
+import Gestion_Productos from "./Pages/Gestion_Producto/Gestion_Productos.js";
 import Modificar_Producto from "./Pages/Modificar_Producto/Modificar_Producto.js";
-import Agregar_Producto from "./Pages/Agregar_Producto/Agregar_Producto.js"
+import Agregar_Producto from "./Pages/Agregar_Producto/Agregar_Producto.js";
 import Header from "./Components/Header/Header.js";
-import { Redirect, Route } from "react-router";
+import { Redirect, Route, Switch } from "react-router";
 import { withRouter } from "react-router";
-
 
 const exclusionPaths = ["/Login", "/Register"];
 
@@ -18,15 +17,21 @@ function App({ location }) {
   return (
     <div>
       {exclusionPaths.indexOf(location.pathname) < 0 && <Header />}
-      <Route exact path="/Register" component={Register} />
-      <Route exact path="/Login" component={Login} />
-      <Route exact path="/" component={Home} />
-      <Route exact path="/Gestion_Productos" component={Gestion_Productos} />
-      <Route exact path="/Modificar_Producto/:id" component={Modificar_Producto} />
-      <Route exact path="/Agregar_Producto/" component={Agregar_Producto}/>
-      <Route exact path="/ProductDetail/:id" component={ProductDetail} />
-      <Route exact path="/Profile/:username" component={Profile} />
-      <Redirect to="/"/>
+      <Switch>
+        <Route exact path="/Register" component={Register} />
+        <Route exact path="/Login" component={Login} />
+        <Route exact path="/Gestion_Productos" component={Gestion_Productos} />
+        <Route
+          exact
+          path="/Modificar_Producto/:id"
+          component={Modificar_Producto}
+        />
+        <Route exact path="/Agregar_Producto/" component={Agregar_Producto} />
+        <Route exact path="/ProductDetail/:id" component={ProductDetail} />
+        <Route exact path="/Profile/:username" component={Profile} />
+        <Route exact path="/" component={Home} />
+        <Redirect to="/" />
+      </Switch>
     </div>
   );
 }
